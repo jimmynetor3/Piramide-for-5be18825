07e764bbbd7b16b6835ac58bc6d8ca149dd7b1dd
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2019-11-28 11:07:04 +0100

    Uitwerking van de exercise

--- piramide.php
+++ piramide.php
@@ -4,9 +4,7 @@
 $number = readline("hoeveel wil je ");
 
 for ($i = 1; $i < $number ; $i++) {
-    for ($j = 1; $j <= $i; $j++) {
-        echo "*";
-    }
+    echo str_repeat("*" , $i);
     echo PHP_EOL;
 }
 ?>
